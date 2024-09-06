284700fbab6440b8d4090482620dbe3b510dc1be
ElectronDragon <electricdragondevelopment@gmail.com>
2024-09-06 08:00:50 +0100

    Write basic #create in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   # Instructed to create empty methods.
   def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprecessable_entity
+    end
   end
 
   def new
