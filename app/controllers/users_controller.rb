a85eaf139e5b6df2da369e6ef917f48e7d5e51a4
ElectronDragon <electricdragondevelopment@gmail.com>
2024-09-09 07:31:23 +0100

    Rewrite original #create params which were deleted rather than commented out; Comment out original #create params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   # Instructed to create empty methods.
   def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
