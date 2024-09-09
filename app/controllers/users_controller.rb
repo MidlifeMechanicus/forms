ac0b0ed05eb32afd42fb8f85c5d40a90ca56823a
ElectronDragon <electricdragondevelopment@gmail.com>
2024-09-09 08:10:27 +0100

    Refactor #form_with to reference user model; Test refactored code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   # Instructed to create empty methods.
+
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
@@ -7,7 +8,8 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to new_user_path
     else
-      render :new, status: :unprocessable_entity
+      render :new
+      # , status: :unprocessable_entity
     end
   end
 
